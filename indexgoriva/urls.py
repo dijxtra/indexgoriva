0cc409ce7bb59ff4c67a2a5a34ff6cca6cf1db06
Nikola Škorić <nskoric@gmail.com>
2015-03-09 06:34:19 +0100

    Malo foolprofinga

--- indexgoriva/urls.py
+++ indexgoriva/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'apps.pregledindexa.views.index', name='home_view'),
+    url(r'^gorivo/$', 'apps.pregledindexa.views.index'),
+    url(r'^indeksi/$', 'apps.pregledindexa.views.index'),
     url(r'^gorivo/(?P<gorivo_id>\d+)$', 'apps.pregledindexa.views.gorivo', name='gorivo_view'),
     url(r'^indeksi/(?P<gorivo_id>\d+)$', 'apps.pregledindexa.views.indeksi', name='indeksi_view'),
     # url(r'^blog/', include('blog.urls')),
