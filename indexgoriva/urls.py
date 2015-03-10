a3cbd062df8081ec3cdecb2d8e60cbf66af0c329
Nikola Škorić <nskoric@gmail.com>
2015-03-10 06:05:22 +0100

    Cleanup of urls.

--- indexgoriva/urls.py
+++ indexgoriva/urls.py
@@ -1,15 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'apps.pregledindexa.views.index', name='home_view'),
-    url(r'^gorivo/$', 'apps.pregledindexa.views.index'),
-    url(r'^indeksi/$', 'apps.pregledindexa.views.index'),
-    url(r'^impressum/$', 'apps.pregledindexa.views.impressum', name='impressum_view'),
-    url(r'^gorivo/(?P<gorivo_id>\d+)$', 'apps.pregledindexa.views.gorivo', name='gorivo_view'),
-    url(r'^indeksi/(?P<gorivo_id>\d+)$', 'apps.pregledindexa.views.indeksi', name='indeksi_view'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('apps.pregledindexa.views',
+    url(r'^$', 'index', name='home_view'),
+    url(r'^gorivo/$', 'index'),
+    url(r'^indeksi/$', 'index'),
+    url(r'^impressum/$', 'impressum', name='impressum_view'),
+    url(r'^gorivo/(?P<gorivo_id>\d+)$', 'gorivo', name='gorivo_view'),
+    url(r'^indeksi/(?P<gorivo_id>\d+)$', 'indeksi', name='indeksi_view'),
+    url(r'^set_limit/(?P<view_name>\w+)/(?P<gorivo_id>\d+)$', 'set_limit', name='set_limit_view'),
 )
